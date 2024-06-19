7cbf7c41c52a30164188b3faba9116672a45c9f7
jojohve <joseph.laluna@outlook.it>
2024-06-19 11:30:50 +0200

    Update TripController.php

--- controllers/TripController.php
+++ controllers/TripController.php
@@ -21,33 +21,6 @@ class TripController
 
     public function updateTrip($id)
     {
-        $trip = new Trip($db);
-
-        $data = json_decode(file_get_contents("php://input"));
-
-        if (
-            !empty($data->Id) &&
-            !empty($data->trip_name) &&
-            !empty($data->availability) &&
-            !empty($data->countries_ids) &&
-            is_array($data->countries_ids)
-        ) {
-            $trip->Id = $data->Id;
-            $trip->trip_name = $data->trip_name;
-            $trip->availability = $data->availability;
-            $trip->countries_ids = $data->countries_ids;
-
-            if ($trip->updateTrip()) {
-                http_response_code(200);
-                echo json_encode(array("message" => "Viaggio aggiornato correttamente."));
-            } else {
-                http_response_code(503);
-                echo json_encode(array("message" => "Impossibile aggiornare il Viaggio."));
-            }
-        } else {
-            http_response_code(400);
-            echo json_encode(array("message" => "Impossibile aggiornare il Viaggio, i dati sono incompleti."));
-        }
         require 'models/Trip.php';
         $data = json_decode(file_get_contents("php://input"), true);
         $result = Trip::updateTrip($id, $data);
